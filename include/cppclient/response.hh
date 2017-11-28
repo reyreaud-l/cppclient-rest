8abbc44bc95ed06648597ea5d75c111c5626367c
Loic Reyreaud <lreyreaud@arista.com>
2017-11-28 17:50:56 +0000

    doc: add Response doxygen

--- include/cppclient/response.hh
+++ include/cppclient/response.hh
@@ -5,26 +5,39 @@
 
 namespace cppclient
 {
+/** @class Response
+ *  @brief Represent a response from a request.
+ */
 class Response
 {
   public:
     Response(std::string, std::string, CURLcode, long);
 
+    /** @brief Return the body of the response
+     */
     std::string inline get_body()
     {
       return this->body;
     }
 
+    /** @brief Return the header of the response
+     */
     std::string inline get_header()
     {
       return this->header;
     }
 
+    /** @brief Return the curlcode form libcurl
+     *  @todo    Check CURLcode in the Connection and either raise an exception or create
+     *           an according response with an explicit error message.
+     */
     CURLcode inline get_curlcode()
     {
       return this->curlcode;
     }
 
+    /** @brief Return the HTTP returncode received
+     */
     long inline get_returncode()
     {
       return this->returncode;
@@ -35,7 +48,6 @@ class Response
     std::string header;
     CURLcode curlcode;
     long returncode;
-
 
 };
 }
